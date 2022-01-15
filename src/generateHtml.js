878c9bea7e8f9af498c39db597bc3a7e4ba7a383
Kelsey Mailau <kelseymailau@gmail.com>
2022-01-15 10:13:15 -0800

    add icons to role subtitles

--- src/generateHtml.js
+++ src/generateHtml.js
@@ -7,7 +7,7 @@ const generateHtml = (employees, teamName) => {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- CSS only -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
         <title>Team Profile Generator</title>
@@ -25,14 +25,18 @@ const generateHtml = (employees, teamName) => {
         let employee = employees[i];
         let getInfo = '';
         let href = '';
+        let getIcon = ''
 
         // Add github, office number, or school to the last list item in the card depending on if the team member is an engineer, manager, or intern
         if (employee.getRole() === 'Engineer') {
-            getInfo = `GitHub: <a href="https://github.com/${employee.gitHub}" target="_blank">${employee.gitHub}</a>`
+            getInfo = `GitHub: <a href="https://github.com/${employee.gitHub}" target="_blank">${employee.gitHub}</a>`;
+            getIcon = '<i class="fas fa-glasses"></i>'
         } else if (employee.getRole() === 'Manager') {
             getInfo = `Office Number: ${employee.officeNumber}`
+            getIcon = '<i class="fas fa-mug-hot"></i>'
         } else if (employee.getRole() === 'Intern') {
             getInfo = `School: ${employee.school}`
+            getIcon = '<i class="fas fa-user-graduate"></i>';
         }
 
         teamProfileHtml +=
@@ -41,7 +45,7 @@ const generateHtml = (employees, teamName) => {
                 <div class="card-body">
                 
                     <h2 class="card-title text-light">${employee.firstName}</h2>
-                    <h3 class="card-subtitle lightbrown-text">${employee.getRole()}</h3>
+                    <h3 class="card-subtitle lightbrown-text">${getIcon} ${employee.getRole()}</h3>
                 </div>
             </div>
             <div class="card-body beige">
